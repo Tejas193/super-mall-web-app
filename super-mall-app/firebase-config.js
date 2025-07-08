094307eaef46cd1b54b0a07a311971bf9000bc33
Tejas193 <tejaschauhan.1912@gmail.com>
2025-07-08 22:06:05 +0530

    change1

--- super-mall-app/firebase-config.js
+++ super-mall-app/firebase-config.js
@@ -1,9 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-// ✅ If you plan to use analytics, use this import:
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 
-// ✅ Your Firebase config
 const firebaseConfig = { apiKey: "AIzaSyB_Ze2Fb0k4Ozn0ncE95k9kMeTSLI28RUE",
   authDomain: "super-mall-app-3da42.firebaseapp.com",
   projectId: "super-mall-app-3da42",
@@ -13,6 +11,5 @@ const firebaseConfig = { apiKey: "AIzaSyB_Ze2Fb0k4Ozn0ncE95k9kMeTSLI28RUE",
   measurementId: "G-4XW0DCMPVB"
 };
 
-// ✅ Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
